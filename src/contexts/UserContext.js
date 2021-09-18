c53560d8c8823f6c1c97c74c2b1d6043da19a197
Mario Olinto <marioolinto@gmail.com>
2021-09-18 15:11:38 -0400

    Criação dos componentes

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,11 +1,14 @@
 /* eslint-disable prettier/prettier */
 import React, {createContext, useReducer } from 'react';
 import { initialState, UserReducer } from '../reducers/UserReducer';
+
 export const UserContext = createContext();
 
 export default ({children}) => {
+    const [state, dispatch] = useReducer(UserReducer, initialState);
+
     return (
-        <UserContext.Provider>
+        <UserContext.Provider value={{state, dispatch}}>
             {children}
         </UserContext.Provider>
     );
--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,13 +1,38 @@
 /* eslint-disable prettier/prettier */
 import React from  'react';
-import { Text } from 'react-native';
-import { Container } from './style';
+import { Container, InputArea, CustomButton, CustomButtonText, SignMessageButton, SignMessageButtonText,
+    SignMessageButtonTextBold } from './style';
 
+import BarberLogo from '../../assets/barber.svg';
+
+import SignInput from '../../components/SignInput';
+import EmailIcon from '../../assets/email.svg';
+import LockIcon from '../../assets/lock.svg';
 
 export default () => {
     return (
         <Container>
-            <Text>SignIn</Text>
+            <BarberLogo width="100%" height="160"/>
+
+            <InputArea>
+                <SignInput
+                    IconSvg={EmailIcon}
+                    placeholder="Digite seu e-mail"
+                />
+                <SignInput
+                    IconSvg={LockIcon}
+                    placeholder="Digite sua senha"
+                />
+
+                <CustomButton>
+                    <CustomButtonText>LOGIN</CustomButtonText>
+                </CustomButton>
+
+                <SignMessageButton>
+                    <SignMessageButtonText>Não possui um cadastro ainda?</SignMessageButtonText>
+                    <SignMessageButtonTextBold>Cadastre-se</SignMessageButtonTextBold>
+                </SignMessageButton>
+            </InputArea>
         </Container>
     );
 };
